1a9c176641cc6089b17cff6a9dd3c270df6b9e00
lauragreen <lollymay@gmail.com>
2019-02-22 16:22:21 +0000

    :wrench: SETUP authorisation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
+import ApolloClient from 'apollo-boost';
 import 'dotenv/config';
 
-const userCredentials = { firstname: 'Robin' };
-const userDetails = { nationality: 'German' };
-
-const user = {
-  ...userCredentials,
-  ...userDetails,
-};
-
-console.log(user);
-
-console.log(process.env.SOME_ENV_VARIABLE);
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${process.env.TOKEN}`,
+      },
+    });
+  },
+});
